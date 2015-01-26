378c9994b1938e49c90e908534895c1f2a3796f0
Adler <nkj20932@hotmail.com>
2015-01-26 10:14:44 +0800

    add validation class

--- app/editor.rb
+++ app/editor.rb
@@ -2,18 +2,22 @@ require 'sinatra'
 require 'json'
 
 class Editor < Sinatra::Base
+	require './app/validation'
+
+	before "/data" do
+	code_array = params[:code]
+	if code_array.class == Array && code_array.length > 0
+		@code_string = code_array.inject("") { |result, line| result += line + ";"}
+	end
+	end
+
 	get "/ace" do
 		erb :'ace.html'
 	end
 
 	post "/data" do
-		code_array = params[:code]
-		code_string = code_array.inject("") { |result, line| result += line + ";"}
-		code_string += "Post.instance_methods.include? :hello;"
-		# a = "-> {#{code_string}}"
-		# result = (eval a).call
-		result = eval code_string
+		# @code_string += "Post.instance_methods.include? :hello;"
+		result = eval @code_string
 		return result.to_json
-		# return code_string
 	end
 end
---
+++ app/validation.rb
@@ -0,0 +1,7 @@
+class Validation
+	def initialize &block
+		@block = block
+	end
+
+
+end
